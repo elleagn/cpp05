639bcef9ef34af99aa611c5488dbf99484042f75
gozon <gozon@student.42.fr>
2025-06-05 06:20:15 +0200

    Add form

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:06:13 by gozon             #+#    #+#             */
-/*   Updated: 2025/05/26 17:33:17 by gozon            ###   ########.fr       */
+/*   Updated: 2025/05/26 18:29:57 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,15 @@ Form::~Form() {
 Form::Form(const Form& src): _name(src._name), _isSigned(src._isSigned), 
 _execGrade(src._execGrade), _signGrade(src._signGrade) {
 
+    try {
+        if (this->_signGrade > 150 || this->_execGrade > 150)
+            throw Form::GradeTooLowException();
+        if (this->_signGrade < 1 || this->_execGrade < 1)
+            throw Form::GradeTooHighException();
+    }
+    catch (std::exception &e) {
+    }
+    
 }
 
 Form::Form(const std::string& name, int execGrade, int signGrade): 
@@ -59,4 +68,12 @@ bool Form::getStatus() const {
     
     return (this->_isSigned);
     
+}
+
+const char* Form::GradeTooLowException::what() const throw() {
+    return ("Exception: grade too low.");
+}
+
+const char* Form::GradeTooHighException::what() const throw() {
+    return ("Exception: grade too high.");
 }
